e3aecdf4096b889d936af2e3ffc2f561cb6e670a
Daniel Holland <danhol1989@gmail.com>
2022-02-27 20:47:16 -0800

    Third commit: added to server.json

--- server.js
+++ server.js
@@ -13,31 +13,55 @@ app.use('/api', api);
 
 app.use(express.static('public'));
 
-app.get('/notes', (req, res) => {
-    res.json(JSON.parse(data));
-})
+function updateDatabase() {
+    fs.writeFile('db/db.json', JSON.stringify(notes, '\t'), err => {
+            if (err) throw err;
+            return true;
+    });
+};
 
-app.get('/api/notes', (req, res) => {
-    //fix this to read db.json, and return notes as JSON
-    res.sendFile(path.join(__dirname, '/db/db.json'));
-})
+// Struggled with this for a couple days.  Took ideas from:
+// https://github.com/amandalatkins/note-taker/blob/master/routes/routes.js
+// in order to complete.
+fs.readFile('db/db.json', 'utf8', (err, data) => {
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-})
+    if(err) throw err;
 
-app.post('/api/notes', (req, res) => {
-    //save request body, add to db.json, and return to client
-    let addNote = req.body;
-    (JSON.parse(data)).push(addNote)
-})
+    let notes = JSON.parse(data);
 
-app.delete('/api/notes/:id', (req, res) => {
-    //delete specific id note by reading notes in db.json, 
-    //remove the note with id, 
-    //and then rewrite the notes to db.json
-})
+    app.get('/notes', (req, res) => {
+        res.json(JSON.parse(data));
+    })
 
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
-);
+    app.get('/api/notes', (req, res) => {
+        //Read db.json, and return notes as JSON
+        res.json(notes);
+    })
+
+    app.get('*', (req, res) => {
+        //catch all reamaining 'get' and go to index.html
+        res.sendFile(path.join(__dirname, '/public/index.html'))
+    })
+
+    app.post('/api/notes', (req, res) => {
+        //save request body, add to db.json, and return to client
+        let addNote = req.body;
+        notes.push(addNote);
+        updateDatabase();
+        console.log(`New note added: ${addNote.title}`);
+    })
+
+    app.delete('/api/notes/:id', (req, res) => {
+        //delete specific id note by reading notes in db.json, 
+        //remove the note with id, 
+        //and then rewrite the notes to db.json
+        notes.splice(req.params.id, 1);
+        updateDatabase();
+        console.log(`Deleted note ${req.params.id}`)
+    })
+
+    app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT} 🚀`)
+    );
+
+});
